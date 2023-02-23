19dd134b8fd09768adcca11e40b5a8fe31b637d7
Animesh <hulknoob15@gmail.com>
2023-02-24 00:04:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Textarea from "./components/Textarea";
+import { useState } from "react";
 
+import { Routes, Route } from "react-router-dom";
 function App() {
+  const dark = {
+    background: "black",
+    color: "white",
+  };
+
+  const light = {
+    background: "white",
+    color: "black",
+  };
+
+  const [toggle, setToggle] = useState(true);
+  const [mode, setMode] = useState(light);
+  const [nameMode, setNameMode] = useState("Dark Mode");
+  const [color, setColor] = useState("light");
+
+  const changeMode = () => {
+    if (toggle) {
+      setToggle(!toggle);
+      setNameMode("Light Mode");
+      setColor("dark");
+      setMode(dark);
+
+      document.body.style.backgroundColor = "black";
+    } else {
+      setToggle(!toggle);
+      setNameMode("Dark Mode");
+      setColor("light");
+      setMode(light);
+
+      document.body.style.backgroundColor = "white";
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={mode}>
+        <Navbar
+          naam={"TextPlay"}
+          color={color}
+          nmode={nameMode}
+          changeMode={changeMode}
+        />
+
+        <Routes>
+          <Route exact path="/" element={<Textarea mode={mode} />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
